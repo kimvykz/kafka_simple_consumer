424383bbc35f1efc36fbfbf1116cfe1b8eccb976
svkim <svkim@dainls.com>
2025-04-04 18:08:34 +0900

    added DLQ

--- src/main/java/com/aisawan/config/KafkaConfig.java
+++ src/main/java/com/aisawan/config/KafkaConfig.java
@@ -1,7 +1,12 @@
 package com.aisawan.config;
 
 import com.aisawan.model.SatelliteEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +15,22 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiFunction;
 
 @EnableKafka
 @Configuration
+@Slf4j
 public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
@@ -38,16 +51,47 @@ public class KafkaConfig {
 //        config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, SatelliteEntity.class);
 //        config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
 
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        config.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory(
+            KafkaTemplate<String, Object> kafkaTemplate
+    ) {
+
+
         ConcurrentKafkaListenerContainerFactory<String, String> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.setConsumerFactory(consumerFactory());
+
+        //only for 1 attempt
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
+        factory.setCommonErrorHandler(errorHandler(kafkaTemplate));
+
         return factory;
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(AdminClientConfig.RETRIES_CONFIG, 3);
+        return new KafkaAdmin(config);
+    }
+
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaTemplate<String, Object> kafkaTemplate) {
+
+        BiFunction<ConsumerRecord<?, ?>, Exception, TopicPartition> dlqResolver =
+                (record, ex) -> new TopicPartition(record.topic() + "-dlq", record.partition());
+
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate, dlqResolver);
+
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(0L, 0L));
     }
 
 }
--- src/main/java/com/aisawan/consumer/SatelliteConsumer.java
+++ src/main/java/com/aisawan/consumer/SatelliteConsumer.java
@@ -6,27 +6,47 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.Message;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class SatelliteConsumer {
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    @Value("${spring.kafka.topics.satellite}")
+    private String topicName;
+
     private final SatelliteRepository satelliteRepository;
+
+//    private void sendToDLQ(Object value) {
+//        try {
+//            kafkaTemplate.send("aisawan-topic-dlq", value);
+//            log.warn("Message has been sent to DLQ: {}", value);
+//        } catch (Exception e) {
+//            log.error("Error sending to Dlq! Data has been lost! ", e);
+//        }
+//    }
 
     @KafkaListener(topics = "#{'${spring.kafka.topics.satellite}'}", groupId = "group_id", containerFactory = "concurrentKafkaListenerContainerFactory")
     public void consume(ConsumerRecord<String, String> record) {
         ObjectMapper objectMapper = new ObjectMapper();
         log.info("--->>>Received value--->>>" + record.value());
 
+        SatelliteEntity satellite = new SatelliteEntity();
+
         try {
-            SatelliteEntity satellite = objectMapper.readValue(record.value(), SatelliteEntity.class);
+            satellite = objectMapper.readValue(record.value(), SatelliteEntity.class);
             satelliteRepository.save(satellite);
+            System.out.println("--->>>Data saved--->>>");
         } catch (Exception e) {
             log.error(e.getMessage());
+            throw new RuntimeException("Error occurred:" + e.getMessage());
         }
 
     }
